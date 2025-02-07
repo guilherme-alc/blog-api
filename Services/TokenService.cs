ec11838c89f3d128c3def251b88665ba58d248e7
guilherme-alc <guilhermealc01@gmail.com>
2025-02-06 22:26:06 -0300

    Inclui claims no token JWT e extensão para roles

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,8 @@
-﻿using Blog.Models;
+﻿using Blog.Extensions;
+using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Blog.Services
@@ -10,17 +12,18 @@ namespace Blog.Services
         public string GenerateToken(User user)
         {
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-
+            var claims = user.GetClaims();
             var tokenHandler = new JwtSecurityTokenHandler();
             // Configuração do token
             var tokenDescriptor = new SecurityTokenDescriptor
             {
+                Subject = new ClaimsIdentity(claims),
+
                 Expires = DateTime.UtcNow.AddHours(8),
 
                 SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(key), 
+                    new SymmetricSecurityKey(key),
                     SecurityAlgorithms.HmacSha256Signature)
-
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
