796f5777eb3333eab5ae464a8c73e425476624de
guilherme-alc <guilhermealc01@gmail.com>
2025-02-09 22:08:04 -0300

    Adiciona novos métodos de leitura com filtro para Post

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -50,5 +50,75 @@ namespace Blog.Controllers
                 return StatusCode(500, new ResultViewModel<List<Post>>("05XE21 Falha interna no servidor"));
             }
         }
+
+        [HttpGet("v1/posts/{id:int}")]
+        public async Task<IActionResult> GetDetailsAsync(
+            [FromServices] BlogDataContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                var post = await context.Posts
+                    .AsNoTracking()
+                    .Include(p => p.Category)
+                    .Include(p => p.Author)
+                    .ThenInclude(a => a.Roles)
+                    .FirstOrDefaultAsync(p => p.Id == id);
+
+                if(post == null)
+                {
+                    return NotFound(new ResultViewModel<string>("05XE22 Post não encontrado"));
+                }
+
+                return Ok(new ResultViewModel<Post>(post));
+
+            }catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<string>("05XE20 Falha interna no servidor"));
+            }
+        }
+
+        [HttpGet("v1/posts/category/{category}")]
+        public async Task<IActionResult> GetByGategoryAsync(
+            [FromServices] BlogDataContext context,
+            [FromRoute] string category,
+            [FromQuery] int page = 0,
+            [FromQuery] int pageSize = 25)
+        {
+            try
+            {
+                var count = await context.Posts.AsNoTracking().CountAsync();
+                var posts = context.Posts
+                    .AsNoTracking()
+                    .Include(p => p.Category)
+                    .Include(p => p.Author)
+                    .Where(p => p.Category.Slug == category)
+                    .Select(p => new ListPostsViewModel()
+                    {
+                        Id = p.Id,
+                        Title = p.Title,
+                        Slug = p.Slug,
+                        LastUpdateDate = p.LastUpdateDate,
+                        Category = p.Category.Name,
+                        Author = $"{p.Author.Name} ({p.Author.Email})"
+                    })
+                    .Skip(page * pageSize)
+                    .Take(pageSize)
+                    .OrderByDescending(p => p.LastUpdateDate)
+                    .ToListAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    total = count,
+                    page,
+                    pageSize,
+                    posts
+                }));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<List<Post>>("05XE21 Falha interna no servidor"));
+            }
+        }
     }
 }
