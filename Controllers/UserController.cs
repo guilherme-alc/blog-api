74e44f2c878d45b92ca3fb5c33e8a890a0b09c80
guilherme-alc <guilhermealc01@gmail.com>
2025-03-07 09:52:04 -0300

    docs: documentação para operações em usuários

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using Blog.Data;
-using Blog.Models;
+﻿using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -16,6 +15,16 @@ namespace Blog.Controllers
         {
             _service = service;
         }
+
+        /// <summary>
+        /// Obter todos os usuários
+        /// </summary>
+        /// <returns>Coleção de usuários</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="401">Não autenticado</response>
+        /// <response code="500">Falha no servidor</response>
+        [ProducesResponseType(typeof(ResultViewModel<IEnumerable<User>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResultViewModel<string>), StatusCodes.Status500InternalServerError)]
         [Authorize(Roles = "admin")]
         [HttpGet("v1/users")]
         public async Task<IActionResult> GetAsync([FromQuery] int page = 0, [FromQuery] int pageSize = 25)
@@ -39,6 +48,15 @@ namespace Blog.Controllers
             }
         }
 
+        /// <summary>
+        /// Obter detalhes de um usuário
+        /// </summary>
+        /// <param name="id">Identificador do usuário</param>
+        /// <returns>Dados do usuário</returns>
+        [ProducesResponseType(typeof(ResultViewModel<User>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResultViewModel<string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResultViewModel<string>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ResultViewModel<string>), StatusCodes.Status500InternalServerError)]
         [Authorize(Roles ="admin")]
         [HttpGet("v1/users/{id:int}")]
         public async Task<IActionResult> GetDetailsAsync([FromRoute] int id)
@@ -75,6 +93,15 @@ namespace Blog.Controllers
             }
         }
 
+        /// <summary>
+        /// Vincular um usuário a uma função
+        /// </summary>
+        /// <response code="204">Sucesso</response>
+        /// <response code="401">Não autenticado</response>
+        /// <response code="404">Não autorizado</response>
+        /// <response code="500">Falha ao vincular usuário</response>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResultViewModel<string>), StatusCodes.Status500InternalServerError)]
         [Authorize(Roles = "admin")]
         [HttpPost("v1/users/{userId:int}/role/{roleId:int}")]
         public async Task<IActionResult> LinkRoleToUser([FromRoute] int userId, [FromRoute] int roleId)
@@ -85,13 +112,7 @@ namespace Blog.Controllers
 
                 var userRoles = user.Roles.Select(r => r.Name).ToList();
 
-                return StatusCode(200, new ResultViewModel<dynamic>(new
-                {
-                    user.Id,
-                    user.Name,
-                    user.Email,
-                    userRoles
-                }));
+                return StatusCode(204);
             }
             catch (ArgumentException ex)
             {
@@ -111,6 +132,15 @@ namespace Blog.Controllers
             }
         }
 
+        /// <summary>
+        /// Desvincular um usuário a uma função
+        /// </summary>
+        /// <response code="204">Sucesso</response>
+        /// <response code="401">Não autenticado</response>
+        /// <response code="404">Não autorizado</response>
+        /// <response code="500">Falha ao desvincular usuário</response>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResultViewModel<string>), StatusCodes.Status500InternalServerError)]
         [Authorize(Roles = "admin")]
         [HttpDelete("v1/users/{userId:int}/role/{roleId:int}")]
         public async Task<IActionResult> DeleteRoleToUser([FromRoute] int userId, [FromRoute] int roleId)
@@ -121,13 +151,7 @@ namespace Blog.Controllers
 
                 var userRoles = user.Roles.Select(r => r.Name).ToList();
 
-                return StatusCode(200, new ResultViewModel<dynamic>(new
-                {
-                    user.Id,
-                    user.Name,
-                    user.Email,
-                    userRoles
-                }));
+                return StatusCode(204);
             }
             catch (ArgumentException ex)
             {
